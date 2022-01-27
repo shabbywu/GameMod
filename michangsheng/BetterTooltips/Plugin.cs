307b1fe10e681fee03b32fd12e9727977cd9a993
shabbywu <shabbywu@qq.com>
2022-01-27 19:13:00 +0800

    feature:实现 StrengthenDongfu 和 BetterTooltips

---
+++ michangsheng/BetterTooltips/Plugin.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using BepInEx;
+using BepInEx.Configuration;
+using HarmonyLib;
+
+namespace BetterTooltips
+{
+    [BepInPlugin("cn.shabywu.michangsheng.BetterTooltips", "更丰富的弹窗信息", "1.0.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        public static ConfigEntry<bool> ShowYaoYin;
+        public static ConfigEntry<bool> ShowChandi;
+
+        public static Harmony _harmony;
+
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+
+            ShowYaoYin = Config.Bind("Tooltip", "ShowYaoYin", false, "是否展示药草可炼制的丹方");
+            ShowChandi = Config.Bind("Tooltip", "ShowChandi", false, "是否展示药草的产地");
+
+            _harmony = Harmony.CreateAndPatchAll(typeof(Plugin));
+            LogDebug = Logger.LogDebug;
+        }
+
+        private void OnDestroy() {
+            _harmony.UnpatchSelf();
+        }
+
+        public delegate void Log(object data);
+        public static Log LogDebug;
+
+
+        [HarmonyPatch(typeof(ToolTipsMag), "UpdateSize")]
+        [HarmonyPrefix]
+        static public void PatchToolTipsMag(ref ToolTipsMag __instance, ref ToolTipsMag.Direction ____direction, ref Dictionary<int, string> ____qualityNameColordit)
+        {
+            Bag.BaseItem item = __instance.BaseItem;
+            // 只要展示的不是草药, 就都不处理
+            if (item == null)
+            {
+                LogDebug("item is null");
+                return;
+            }
+
+            if (item.ItemType == Bag.ItemType.草药)
+            {
+                CaoYaoHandler.ShowPossibleDanFang(ref __instance, ref ____direction, ref ____qualityNameColordit);
+                if (ShowChandi.Value) {
+                    CaoYaoHandler.ShowPossibleChanDi(ref __instance, ref ____direction);
+                }
+            }
+        }
+    }
+}
---
+++ michangsheng/StrengthenDongfu/Plugin.cs
@@ -0,0 +1,59 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using HarmonyLib;
+using System.Collections.Generic;
+
+namespace StrengthenDongfu
+{
+    [BepInPlugin("cn.shabywu.michangsheng.StrengthenDongfu", "洞府加强", "0.1.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        static ConfigEntry<int> ZYXiuLianMultipiler;
+        static ConfigEntry<int> ZYLingTianMultipiler;
+        static ConfigEntry<int> ZYLingTianCuiShengMultipiler;
+
+        static ConfigEntry<int> LYXiuLianMultipiler;
+        static ConfigEntry<int> LYLingTianMultipiler;
+
+        public delegate void Log(object data);
+        public static Log LogDebug;
+
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+
+            ZYXiuLianMultipiler = Config.Bind("ZhenYan", "ZYXiuLianMultipiler", 2, "【阵眼】修炼加成倍率");
+            ZYLingTianMultipiler = Config.Bind("ZhenYan", "ZYLingTianMultipiler", 3, "【阵眼】灵田培养加成倍率");
+            ZYLingTianCuiShengMultipiler = Config.Bind("ZhenYan", "ZYLingTianCuiShengMultipiler", 5, "【阵眼】灵田小绿瓶催生加成倍率");
+
+            LYXiuLianMultipiler = Config.Bind("LingYan", "LYXiuLianMultipiler", 2, "【灵眼】修炼加成倍率");
+            LYLingTianMultipiler = Config.Bind("LingYan", "LYLingTianMultipiler", 3, "【灵眼】灵田培养加成倍率");
+
+            Harmony.CreateAndPatchAll(typeof(Plugin));
+
+            LogDebug = Logger.LogDebug;
+        }
+
+        // 提高【阵眼】相关的数据数值
+        [HarmonyPatch(typeof(JSONClass.DFZhenYanLevel), "InitDataDict")]
+        [HarmonyPostfix]
+        public static void  PatchDFZhenYanLevelInitDataDict(ref Dictionary<int, JSONClass.DFZhenYanLevel> ___DataDict) {
+            foreach (var kv in ___DataDict) {
+                kv.Value.xiuliansudu *= ZYXiuLianMultipiler.Value;
+                kv.Value.lingtiansudu *= ZYLingTianMultipiler.Value;
+                kv.Value.lingtiancuishengsudu *= ZYLingTianCuiShengMultipiler.Value;
+            }
+        }
+
+        // 提高【灵眼】相关的数据数值
+        [HarmonyPatch(typeof(JSONClass.DFLingYanLevel), "InitDataDict")]
+        [HarmonyPostfix]
+        public static void  PatchDFLingYanLevelInitDataDict(ref Dictionary<int, JSONClass.DFLingYanLevel> ___DataDict) {
+            foreach (var kv in ___DataDict) {
+                kv.Value.xiuliansudu *= LYXiuLianMultipiler.Value;
+                kv.Value.lingtiansudu *= LYLingTianMultipiler.Value;
+            }
+        }
+    }
+}
