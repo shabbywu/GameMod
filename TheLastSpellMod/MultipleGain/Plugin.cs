5005ccd55a7f7ebbbcb3779510ac9c58db34596e
shabbywu <shabbywu@tencent.com>
2023-10-07 14:06:41 +0800

    TheLastSpellMod/MultipleGain 支持调整金币和材料的获取倍率

--- TheLastSpellMod/MultipleGain/Plugin.cs
+++ TheLastSpellMod/MultipleGain/Plugin.cs
@@ -1,7 +1,8 @@
-﻿using System;
+using System;
 using BepInEx;
 using BepInEx.Configuration;
 using HarmonyLib;
+using TheLastStand.Controller.Panic;
 using TheLastStand.Model.Unit;
 using TheLastStand.Manager;
 
@@ -12,6 +13,8 @@ namespace MultipleGain
     {
         static ConfigEntry<float> ExperiencePercentage;
         static ConfigEntry<float> DamnedSoulsPercentage;
+        static ConfigEntry<float> PanicRewardGoldPercentage;
+        static ConfigEntry<float> PanicRewardMaterialsPercentage;
 
         private void Awake()
         {
@@ -20,10 +23,12 @@ namespace MultipleGain
 
             ExperiencePercentage = Config.Bind("TheLastStand.MultipleGain",  "ExperiencePercentage", 1.5f, "经验倍率");
             DamnedSoulsPercentage = Config.Bind("TheLastStand.MultipleGain",  "DamnedSoulsPercentage", 10f, "污秽精华倍率");
+            PanicRewardGoldPercentage = Config.Bind("TheLastStand.MultipleGain",  "PanicRewardGoldPercentage", 1.0f, "金币倍率");
+            PanicRewardMaterialsPercentage = Config.Bind("TheLastStand.MultipleGain",  "PanicRewardMaterialsPercentage", 1.0f, "材料倍率");
             Harmony.CreateAndPatchAll(typeof(Plugin));
         }
 
-        [HarmonyPatch(typeof(Unit), "ExperienceGain", MethodType.Getter)] // Specify target method with HarmonyPatch attribute
+        [HarmonyPatch(typeof(Unit), "ExperienceGain", MethodType.Getter)]
         [HarmonyPostfix]
         static void patchUnitExperienceGain(ref float __result)
         {
@@ -31,13 +36,21 @@ namespace MultipleGain
             Console.WriteLine($"patch ExperienceGain: {__result}");
         }
 
-        [HarmonyPatch(typeof(TrophyManager), "AddEnemyKill")] // Specify target method with HarmonyPatch attribute
+        [HarmonyPatch(typeof(TrophyManager), "AddEnemyKill")]
         [HarmonyPrefix]
         static bool patchTrophyManagerAddEnemyKill(ref int damnedSoulsEarned)
         {
             damnedSoulsEarned = Convert.ToInt32(damnedSoulsEarned * DamnedSoulsPercentage.Value);
             Console.WriteLine($"patch AddEnemyKill: {damnedSoulsEarned}");
             return true;
+        }
+
+        [HarmonyPatch(typeof(PanicRewardController), "GetReward")]
+        [HarmonyPostfix]
+        static void patchGetReward(ref PanicRewardController __instance)
+        {
+            __instance.PanicReward.Gold = Convert.ToInt32(__instance.PanicReward.Gold * PanicRewardGoldPercentage.Value);
+            __instance.PanicReward.Materials = Convert.ToInt32(__instance.PanicReward.Materials * PanicRewardMaterialsPercentage.Value);
         }
     }
 
